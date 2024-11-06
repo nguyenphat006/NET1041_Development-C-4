ffc29a1ecc8b8fc7240b8d5f951bf03e1ea1e916
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2024-11-06 09:21:57 +0700

    Add project files.

---
+++ C#4_Lab01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C_4_Lab01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
