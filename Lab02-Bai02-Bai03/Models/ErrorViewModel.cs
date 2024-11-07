095fe02681506973b23f419da35bffe24cb47fb6
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2024-11-07 22:27:36 +0700

    Update

---
+++ Lab02-Bai02-Bai03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab02_Bai02_Bai03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Lab02-Bai02-Bai03/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Middleware 1
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("<div>Hello FPoly from the middleware 1</div>");
+    await next.Invoke();
+    await context.Response.WriteAsync("<div>Returning from the middleware 1</div>");
+});
+
+// Middleware 2
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("<div>Hello FPoly from the middleware 2</div>");
+    await next.Invoke();
+    await context.Response.WriteAsync("<div>Returning from the middleware 2</div>");
+});
+
+// Middleware 3 - dùng Run để dừng chuỗi middleware tại đây
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("<div>Hello FPoly from the middleware 3</div>");
+});
+
+app.Run();
